923a98143e7c817295ef7547047db88ae09e86c6
s8370 <s8370@pjwstk.edu.pl>
2020-03-01 19:30:37 +0100

    Dodałem HTTP

--- Cwiczenia1/Cwiczenia1/Program.cs
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Cwiczenia1
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HttpClient client = new HttpClient();
+            HttpResponseMessage result = await client.GetAsync("https://www.pja.edu.pl");
+            if (result.IsSuccessStatusCode)
+            {
+                string html = await result.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z0-9]+@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+                var matches = regex.Matches(html);
+                foreach(var m in matches)
+                {
+                    Console.WriteLine(m);
+                }
+            }
+
+            //JS Promis asnyc/await
+            // Java Future
+            // C# Task<T> async/await
+
+
+            string str = "ss";
+            //NazwaMetody
+            Console.WriteLine("Koniec!");
         }
     }
 }
